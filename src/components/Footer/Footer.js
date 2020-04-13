1ac63dfc61f71dabbf7f71b4515b7cb7db6451e8
Matt Halvorson <halvorson34.mh@gmail.com>
2020-04-13 11:35:43 -0500

    added header and footer with props

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Footer extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Have fun!</h3>
+        <p>Don't forget to mind your Ps and Qs!</p>
+      </div>
+    );
+  }
+}
+
+export default Footer;
